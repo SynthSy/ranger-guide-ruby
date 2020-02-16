c932b256077d22ae7c6cd314d1c625e252a5bd64
Unknown <synthsy@gmail.com>
2020-02-15 16:11:49 -0800

    better scroll to top

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,21 @@ $(function () {
 $(window).scroll(function(){
     $(".jumbotron").css("opacity", 1 - $(window).scrollTop() / 250);
     });
+	
+/* Scroll to Top */
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
